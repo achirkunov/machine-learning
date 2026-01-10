dd71cadbfd6d69a381d054924a39206775915f0f
Anton Chirkunov <41851+achirkunov@users.noreply.github.com>
2026-01-10 17:53:04 +0000

    Add Matrix struct with basic operations
    Learning Rust: zeros, fill, filled, add functions

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,58 @@
-mod tensor;
-mod activation;
-mod loss;
-mod layer;
-mod optim;
 
-use tensor::Tensor;
+#[derive(Debug)] // Auto-generates code to print the struct
+struct Matrix {
+    rows: usize, // TODO: might need to use u32 for memory optimization for millions of small matrices
+    cols: usize,
+    data: Vec<f32>,
+}
+
+impl Matrix {
+    // Advatanges of new method:
+    // Validation
+    // Encapsulation
+    pub fn zeros(rows: usize, cols: usize) -> Self {
+        // Names don't match so need to add data
+        Self { rows, cols, data: vec![0.0; rows*cols]}
+    }
+
+    pub fn clear(&mut self) {
+        // Mutates matrix in place
+        todo!()
+    }
+
+    // The idiomatic Rust pattern is:
+    // clear(&mut self) - mutates in place
+    pub fn fill(&mut self, x: f32) {
+        self.data.fill(x);
+    }
+    // cleared(&self) -> Matrix returns new
+    pub fn filled(&self, x: f32) -> Self {
+        // Only use the explicit name (Matrix) when returning a different type
+        Self { rows:self.rows, cols:self.cols, data: vec![x; self.rows*self.cols]}
+    }
+
+    pub fn add(a: &Matrix, b: &Matrix, out: &mut Matrix) {
+        for i in 0..a.data.len() {
+            out.data[i] = a.data[i] + b.data[i];
+        }
+    }
+}
 
 fn main() {
     println!("ML from scratch in Rust!\n");
 
-    // Create some tensors
-    let t1 = Tensor::randn(vec![3, 3]);
-    println!("Random 3x3 tensor: {:?}", t1);
+    let m = Matrix::zeros(2,2);
 
-    let t2 = Tensor::zeros(vec![2, 4]);
-    println!("Zeros 2x4 tensor: {:?}", t2);
+    println!("{:?}", m);
 
-    let t3 = Tensor::new(vec![1.0, 2.0, 3.0, 4.0], vec![2, 2]);
-    println!("Custom 2x2 tensor: {:?}", t3);
+    let m2 = m.filled(1.0);
+    println!("{:?}", m2);
+
+    let mut m3 = Matrix::zeros(2,2);
+    m3.fill(2.0);
+    println!("{:?}", m3);
+
+    let mut d = Matrix::zeros(2,2);
+    Matrix::add(&m2,&m3, &mut d);
+    println!("{:?}", d);
 }
